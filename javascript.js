4e7f73601ea2d83939b7cd550ebe202787ce6c1a
Antonio <avps2@aluno.ifal.edu.br>
2022-11-28 22:04:33 -0300

    att
    att 3.9

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 let idReceberNome = document.getElementById("idReceberNome")
+let idReceberNomeConversa = document.getElementById("idReceberNomeConversa")
+
 
 function funcaoteste () {
     let nome = idReceberNome.value;
@@ -14,3 +16,32 @@ alert("Solicitação de amizade enviada com sucesso para: " + nome)
 idReceberNome.value="";
 
 }
+
+function funcaoteste2 () {
+    let nome2 = idReceberNomeConversa.value;
+
+if (nome2 == ""){
+    alert("Preenchimento da conversa é obrigatório para localizar");
+    idReceberNomeConversa.focus();
+    return;
+}
+alert('Conversa não encontrada: "' + nome2 +'"')
+
+
+idReceberNomeConversa.value="";
+
+}
+idReceberNomeConversa.addEventListener("keypress" , function(event)
+  {if (event.keyCode == 13){
+   
+    funcaoteste2();
+} 
+}
+  )
+idReceberNome.addEventListener("keypress" , function(event)
+  {if (event.keyCode == 13){
+    
+    funcaoteste();
+} 
+}
+  )
